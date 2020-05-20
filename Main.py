2dcce5de20d730ae5f0cf7dcd9547a1263318347
ankurdivekar <ankur.divekar@gmail.com>
2020-05-20 15:48:36 +0530

    promo_mode now split into 1) general_promo_mode : send same set of files 2) custom_promo_mode : send 1 customized file
    Emails are only sent to ids marked with a 'Yes' in the column names promo_code in the email_list_xlsx

--- Main.py
+++ Main.py
@@ -7,14 +7,6 @@ import os
 import shutil
 import time
 
-simulate_emails = True
-promo_mode = True
-
-email_list_xlsx =Path(r'D:\TechWork\DJAV\Gmail_Automation\EmailList.xlsx')
-email_list_xlsx_simulated =Path(r'D:\TechWork\DJAV\Gmail_Automation\EmailList_Simulated.xlsx')
-email_config_xlsx =Path(r'D:\TechWork\DJAV\Gmail_Automation\EmailConfig.xlsx')
-promo_dir = Path(r'D:\TechWork\DJAV\SongoBingo\Files\GeneratedGames\Games_200508_IN\Game_200508_IN_3')
-
 
 def get_clean_attrib(attrib):
     if type(attrib) == str:
@@ -23,111 +15,129 @@ def get_clean_attrib(attrib):
         return ''
 
 
-# Define directories for generated and sent cards
-cards_dir = promo_dir / 'Cards'
-sent_cards_dir = promo_dir / 'SentCards'
-# Make directory if it doesnt exist
-sent_cards_dir.mkdir(parents=True, exist_ok=True)
+simulate_emails = True
+general_promo_mode = True
+custom_promo_mode = False
+promo_code = 'PromoTest'
 
-# Get promo code
-promo_code = promo_dir.stem
+email_list_xlsx = Path(r'D:\TechWork\DJAV\Gmail_Automation\EmailList.xlsx')
+email_list_xlsx_simulated = Path(r'D:\TechWork\DJAV\Gmail_Automation\EmailList_Simulated.xlsx')
+email_config_xlsx = Path(r'D:\TechWork\DJAV\Gmail_Automation\EmailConfig.xlsx')
+
+custom_promo_dir = Path(r'D:\TechWork\DJAV\SongoBingo\Files\GeneratedGames\Games_200508_IN\Game_200508_IN_5\Cards')
+general_promo_dir = Path(r'D:\TechWork\DJAV\SongoBingo\Files\GeneratedGames\Games_200508_IN\Game_200508_IN_3\Cards')
+sent_promo_dir = Path(r'D:\TechWork\DJAV\SongoBingo\Files\GeneratedGames\Games_200508_IN\Game_200508_IN_5\SentCards')
+
+# Make sent promo directory if it doesnt exist
+sent_promo_dir.mkdir(parents=True, exist_ok=True)
 
 # Get list of cards
-promo_cards_list = os.listdir(cards_dir)
+custom_promo_list = os.listdir(custom_promo_dir)
+general_promo_list = os.listdir(general_promo_dir)
 
 # Get the email list
-df = pd.read_excel(email_list_xlsx, sheet_name='Email List')
+df = pd.read_excel(email_list_xlsx)
 
 # Get list of text for email subject and body
 email_body_list = pd.read_excel(email_config_xlsx, sheet_name='Email body')['Messages'].tolist()
 email_subject_list = pd.read_excel(email_config_xlsx, sheet_name='Email subject')['Subjects'].tolist()
 
 # Check if enough promo assets are available
-if not promo_mode:
-    if len(promo_cards_list) < len(df['Email']):
-        raise Exception('Insufficient number of promo assets. Please run with promo mode ON')
+emails_to_send = df[promo_code].str.contains('yes', case=False).sum()
+if emails_to_send > 0:
+    if custom_promo_mode:
+        if len(custom_promo_list) < emails_to_send:
+            raise Exception('Insufficient number of custom promo assets.')
+else:
+    raise Exception('No emails to send')
 
-# If game code is not present in XLS
+# If game code is not present in XLS raise error
 if promo_code not in df.columns:
-    # Insert game code column
-    df.insert(len(df.columns), promo_code, 'None')
+    raise Exception(f'Column with promocode not found in XLS: {promo_code}')
 
 # Init
 if not simulate_emails:
     ezgmail.init()
 
+# Define visual separators
+separator_1 = 25 * "~~"
+separator_2 = 25 * "*~"
+
 # Send emails one by one
 card_counter = 0
-
+email_counter = 0
 for index, row in df.iterrows():
     firstname = get_clean_attrib(row["First Name"])
     lastname = get_clean_attrib(row['Last Name'])
     email_id = row['Email'].strip()
-    card = row[promo_code]
+    status = row[promo_code]
 
-    if card in promo_cards_list:
-        print(f'Email already sent to {firstname} {lastname}')
-    else:
+    if type(status) == str:
+        status = status.strip().lower()
+
+    if status == 'yes':
+        # Send email
         email_subject = random.choice(email_subject_list).format(firstname=firstname, lastname=lastname)
         email_body = random.choice(email_body_list).format(firstname=firstname, lastname=lastname)
 
         # Handle the carriage returns
         email_body = codecs.decode(email_body, 'unicode_escape')
 
+        # Create attachment list
+        email_attachment_paths = []
+        email_attachment_files = []
+
         # Get the card to attach
-        if promo_mode:
-            if len(promo_cards_list) > 1:
-                gen_card = ', '.join(promo_cards_list)
-            else:
-                gen_card = promo_cards_list[0]
-        else:
-            sent_cards_list = df[promo_code].tolist()
+        if custom_promo_mode:
+            sent_promo_list = df[promo_code].tolist()
             card_not_found = True
             while card_not_found:
-                if card_counter >= len(promo_cards_list):
+                if card_counter >= len(custom_promo_list):
                     raise Exception('Insufficient number of cards!')
                 else:
-                    gen_card = promo_cards_list[card_counter]
-                    if gen_card in sent_cards_list:
+                    gen_promo = custom_promo_list[card_counter]
+                    if gen_promo in sent_promo_list:
                         card_counter += 1
                     else:
                         card_not_found = False
 
-        # Update dataframe
-        df.iloc[index, df.columns.get_loc(promo_code)] = gen_card
-        gen_card_path = cards_dir / gen_card
-
-        if promo_mode:
-            # Set as attachment for email
-            email_attachments = [str(promo_dir / 'Cards' / p) for p in promo_cards_list]
-        else:
             # Rename card with added first name
-            sent_card = gen_card.replace('.png', '') + f'_{firstname}{lastname}.png'.replace(' ', '')
-            sent_card_path = sent_cards_dir / sent_card
+            sent_promo = gen_promo.replace('.png', '') + f'_{firstname}{lastname}.png'.replace(' ', '')
+            sent_promo_path = sent_promo_dir / sent_promo
+            gen_promo_path = custom_promo_dir / gen_promo
 
             # Copy to sent cards folder
-            shutil.copy(gen_card_path, sent_card_path)
+            shutil.copy(gen_promo_path, sent_promo_path)
 
             # Set as attachment for email
-            email_attachments = [str(sent_card_path)]
+            email_attachment_paths += [str(sent_promo_path)]
+            email_attachment_files.append(sent_promo)
+
+            # Update dataframe
+            df.iloc[index, df.columns.get_loc(promo_code)] = gen_promo
+
+        if general_promo_mode:
+            # Set as attachment for email
+            email_attachment_paths += [str(general_promo_dir / p) for p in general_promo_list]
+            email_attachment_files += general_promo_list
 
         if simulate_emails:
-            print(f'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n'
+            print(f'{separator_1}\n'
                   f'Sending email to: {firstname} {lastname}\n'
                   f'at {email_id}\n'
                   f'with Subject: {email_subject}\n'
                   f'and body:\n{email_body}\n'
-                  f'with {len(email_attachments)} attachments:\n{email_attachments}\n'
-                  f'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+                  f'with {len(email_attachment_files)} attachments:\n{email_attachment_files}\n'
+                  f'{separator_1}')
         else:
-            print(f'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n'
+            print(f'{separator_1}\n'
                   f'Sending email to: {email_id}\n'
-                  f'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+                  f'{separator_1}')
             ezgmail.send(
                 recipient=email_id,
                 subject=email_subject,
                 body=email_body,
-                attachments=email_attachments)
+                attachments=email_attachment_paths)
 
         # Write back to xlsx
         if simulate_emails:
@@ -135,8 +145,13 @@ for index, row in df.iterrows():
         else:
             df.to_excel(email_list_xlsx, sheet_name='Email List', index=False)
 
+        # Increment count of emails sent
+        email_counter += 1
+
         # Insert random delay
         if not simulate_emails:
             time.sleep(random.randint(2, 5))
 
-print('Done')
+print(f'\n\n{separator_2}\n'
+      f'Status report:\nFinished sending {email_counter} emails!'
+      f'\n{separator_2}')
